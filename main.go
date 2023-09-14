a5709a0fa807659c4a12dacb02972849e2561370
TimWhite <36320909+timwhitez@users.noreply.github.com>
2023-09-14 14:33:31 +0800

    Add files via upload

--- main.go
+++ main.go
@@ -5,8 +5,10 @@ import (
 	"flag"
 	"fmt"
 	"github.com/alitto/pond"
+	"io/ioutil"
 	"log"
 	"net"
+	"strings"
 	"time"
 )
 
@@ -15,39 +17,78 @@ var dnsr = ""
 func main() {
 
 	threads := 10
+	filename := ""
 	fastUsage := "lookup PTR threads"
 	flag.IntVar(&threads, "t", 10, fastUsage)
 	flag.StringVar(&dnsr, "r", "", "input dns host")
+	flag.StringVar(&filename, "f", "", "input cidr targets filename")
 
 	flag.Parse()
 
 	args := flag.Args()
-	if len(args) != 1 {
+	if len(args) != 1 && filename == "" {
 		log.Fatal("Required argument \"NET/MASK\" missing")
 	}
-	block := args[0]
+	pool := pond.New(threads, 0, pond.MinWorkers(1))
+	if len(args) == 1 {
+		block := args[0]
+		//fmt.Printf("Input Cidr: %s\n", block)
+		ip, ipnet, err := net.ParseCIDR(block)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for ip0 := ip.Mask(ipnet.Mask); ipnet.Contains(ip0); incIP(ip0) {
+			myip := ip0.String()
+			if dnsr == "" {
+				pool.Submit(func() {
+					revIP(myip)
+				})
+			} else {
+				pool.Submit(func() {
+					CusrevIP(myip)
+				})
+			}
 
-	ip, ipnet, err := net.ParseCIDR(block)
-	if err != nil {
-		log.Fatal(err)
+		}
 	}
 
-	pool := pond.New(threads, 0, pond.MinWorkers(1))
-
-	for ip := ip.Mask(ipnet.Mask); ipnet.Contains(ip); incIP(ip) {
-		myip := ip.String()
-		if dnsr == "" {
-			pool.Submit(func() {
-				revIP(myip)
-			})
-		} else {
-			pool.Submit(func() {
-				CusrevIP(myip)
-			})
+	if filename != "" {
+		cidrs := getContent(filename)
+		for _, block := range cidrs {
+			if block == "" {
+				continue
+			}
+			//fmt.Printf("Input Cidr: %s\n", block)
+			ip, ipnet, err := net.ParseCIDR(block)
+			if err != nil {
+				log.Fatal(err)
+			}
+			for ip0 := ip.Mask(ipnet.Mask); ipnet.Contains(ip0); incIP(ip0) {
+				myip := ip0.String()
+				if dnsr == "" {
+					pool.Submit(func() {
+						revIP(myip)
+					})
+				} else {
+					pool.Submit(func() {
+						CusrevIP(myip)
+					})
+				}
+			}
 		}
 
 	}
 	pool.StopAndWait()
+}
+
+func getContent(filename string) []string {
+	content, err := ioutil.ReadFile(filename)
+	if err == nil {
+		str := strings.Replace(string(content), "\r\n", "\n", -1)
+		lines := strings.Split(str, "\n")
+		return lines
+	}
+	return nil
 }
 
 func CusrevIP(ip string) {
